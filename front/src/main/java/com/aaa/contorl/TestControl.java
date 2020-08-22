8cf3f726c49f7f672eb470121d1f536b84a69bdf
Myname <1311776589@qq.com>
2020-08-22 16:03:09 +0800

    初始化项目

---
+++ front/src/main/java/com/aaa/contorl/TestControl.java
@@ -0,0 +1,15 @@
+package com.aaa.contorl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestControl {
+    @RequestMapping("test")
+    @ResponseBody
+    public String show(){
+        return "ok";
+    }
+}
